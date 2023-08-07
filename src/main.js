343818cc43cda9b3fc98514fb074e32efdf90c06
firman <firmanazharrnutech@gmail.com>
2023-08-07 12:58:14 +0700

    add host listen

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { web } from "./application/web.js";
 import { logger } from "./application/logging.js";
 
-web.listen(3000, () => {
+const port = process.env.PORT || 3000
+
+web.listen(port, "0.0.0.0", () => {
   logger.info("App Started");
 });
